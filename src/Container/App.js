9059659e2233d4cbedb944b9f2b568cf5c1b6aa1
MatheusMartinello <matheusfontoura0@gmail.com>
2020-09-20 21:15:41 -0300

    2nd commit

--- src/Container/App.js
+++ src/Container/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import CardList from "../Components/CardList/CardLists";
 import SearchBox from "../Components/searchField/SearchField";
 import Scroll from "../Components/Scroll/Scroll";
+import ErrorBoundry from "../Components/ErrorBoundry/ErrorBoundry";
 class App extends React.Component {
   constructor() {
     super();
@@ -20,23 +21,24 @@ class App extends React.Component {
     this.setState({ searchField: event.target.value });
   };
   render() {
-    const filterRobot = this.state.robots.filter((robots) => {
-      return robots.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const { robots, searchField } = this.state;
+
+    const filterRobot = robots.filter((robots) => {
+      return robots.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    if (this.state.robots.length === 0)
-      return <h1 className="tc">Loading ...</h1>;
-    else
-      return (
-        <div className="App">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
+    return !robots.length ? (
+      <h1 className="tc">Loading ...</h1>
+    ) : (
+      <div className="App">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
             <CardList robots={filterRobot} />
-          </Scroll>
-        </div>
-      );
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
   }
 }
 
